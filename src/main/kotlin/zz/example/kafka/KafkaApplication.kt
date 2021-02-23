8a2207ba33f2ccd263f3c3b06aea4d405a83950f
luzu <luzuspam@gmail.com>
2021-02-23 21:22:16 +0100

    One by one In Parallel When no subscribers then health down

--- src/main/kotlin/zz/example/kafka/KafkaApplication.kt
+++ src/main/kotlin/zz/example/kafka/KafkaApplication.kt
@@ -34,33 +34,6 @@ data class SomeData(val id: String, val data: String)
 @Configuration
 class KafkaConsumerApp {
 
-
-
-//    @Bean
-//    fun myConsumer() = Function<List<SomeData>, Mono<Void>> {
-//        Flux.fromIterable(it)
-//            .doOnNext { println(it) }
-//            .then()
-//    }
-
-//    @Bean
-//    fun myConsumer() = Consumer<Message<SomeData>> { row ->
-//        val tName = Thread.currentThread().name
-//
-//        println("Received")
-//        println("Processing")
-//        TimeUnit.SECONDS.sleep(2)
-//        println("Processed")
-//
-////        Mono.just(row)
-////            .doOnNext { println("Received ${tName}") }
-////            .flatMap { println("Processing ${tName}")
-////                Mono.just(it).delayElement(Duration.ofSeconds(2))
-////            }
-////            .doOnNext { println("Processed ${tName}") }
-////            .subscribe()
-//    }
-
     //processing one by one
     @Bean
     fun myConsumer2() = Function<Flux<Message<SomeData>>, Mono<Void>> { stream ->
@@ -74,30 +47,26 @@ class KafkaConsumerApp {
                         Mono.just(it).delayElement(Duration.ofSeconds(2))
                     }.doOnSuccess {
                         println("Doing commit ${Thread.currentThread().name}")
-//                        (batch.headers[KafkaHeaders.ACKNOWLEDGMENT] as Acknowledgment).acknowledge()
                     }
             }, 0)
             .doOnNext { println("Processed ${Thread.currentThread().name}") }
             .then()
     }
 
-    //processing in parallel (number of cores) - if ack after processing success then ackMode should be MANUAL
-    val al: AtomicLong = AtomicLong()
 
+    //processing in parallel
     @Bean
     fun myConsumer3() = Function<Flux<Message<SomeData>>, Mono<Void>> { stream ->
         stream
-            .limitRequest(1)
             .index()
             .doOnRequest { println("Requested $it") }
-//            .doOnNext { println("Start processing ${Thread.currentThread().name}") }
             .flatMapSequential ({ batch ->
                 Mono.just(batch)
                     .doOnNext { println("Processing ${batch.t1} -> ${Thread.currentThread().name}") }
                     .flatMap {
                         Mono.just(it).delayElement(Duration.ofSeconds(5))
                     }
-            }, 1) //default c=256 p=32
+            }, 1)
             .doOnNext {
                 println("Processed ${it.t1} -> ${Thread.currentThread().name}")
                 (it.t2.headers[KafkaHeaders.ACKNOWLEDGMENT] as Acknowledgment).acknowledge()
@@ -106,6 +75,7 @@ class KafkaConsumerApp {
     }
 
 
+    //just for explosion
     @Bean
     fun myConsumer() = Function<Flux<Message<SomeData>>, Mono<Void>> { stream ->
         stream
